0c656dc2772fe4a575eee42b5e376d3712a5b36d
unknown <thiago.peres@Thiago0685.aorta.lan>
2014-07-09 11:27:21 -0300

    go

--- app/scripts/directives/avatar.js
+++ app/scripts/directives/avatar.js
@@ -5,13 +5,15 @@
             restrict: 'E',
             scope: { module: '@', photo: '@', scale: '@', class: '@'}, //, options: '=', placeholder: '@', ngModel: '=' 
             templateUrl:"views/directives/avatar.html",
-            compile: function(tElem,attrs) {
+            compile: function(tElem,attrs) {             
                 //do optional DOM transformation here
                 return function(scope,elem,attrs) {
-                    $.get(attrs.src)
-                    .fail(function() {
-                    $("[src='"+attrs.src+"']").attr("src", "images/uploads/"+scope.module+"/default.png");
-                    });  
+                      scope.$watch('photo', function(newValue) {
+                        $.get(attrs.src)
+                            .fail(function() {
+                                $("[src='"+attrs.src+"']").attr("src", "images/uploads/"+scope.module+"/default.png");
+                            });  
+                      }, true);   
                 }
             }      
 
