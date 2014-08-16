62bc2165f87d90c1cfbf3e11ddba590a6bae62d0
gpferreira <gustavoferreira@MacBook-Pro-de-Gustavo.local>
2014-08-16 13:20:17 -0300

    go

--- app/scripts/util.js
+++ app/scripts/util.js
@@ -2,8 +2,8 @@
     .factory('Config', function() {
         return {
             getApiUrl : function() {
-            	return 'http://127.0.0.1:1313/api';
-                //return 'http://beet-api.herokuapp.com/api';
+            	//return 'http://127.0.0.1:1313/api';
+                return 'http://beet-api.herokuapp.com/api';
                 //return 'http://201.87.228.140:1313/api';
 
             }
