5bba8f464c768949ac074f7845644435cfda3cdd
thiagokroger <thiagokroger@gmail.com>
2014-11-05 14:15:00 -0200

    go

--- app/scripts/controllers/expense.js
+++ app/scripts/controllers/expense.js
@@ -40,7 +40,11 @@ BeetApp
         $scope.formatDateToDefault = function(date){
             return formatDate(date,"dd/mm/yyyy");
         };
-
+        $scope.formatDateToLabel = function(date){
+            arrDate = date.split("-");
+            var arr = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"];
+            return arr[arrDate[1]-1] + " / " + arrDate[0];
+        };
 
 
         $scope.save = function() {
@@ -98,13 +102,10 @@ BeetApp
             clearExpenseDialog();
         };
 
-        function loadData(){
+        function loadData(initialDate, finalDate){
             if ($scope._id !== undefined){
                 var now = new Date();
-                var initialDate;
-                var finalDate;
-
-                if ($scope.initialDate==undefined){
+                if (initialDate==undefined){
                     initialDate = now.getFullYear();
                     if ((now.getMonth()+1).toString().length==1){
                         initialDate = initialDate + "-" + "0" + (parseInt(now.getMonth())+1).toString();
@@ -112,11 +113,9 @@ BeetApp
                         initialDate = initialDate + "-" + (parseInt(now.getMonth())+1).toString();
                     }
                     initialDate = initialDate + "-01";
-                }else{
-                    initialDate = $scope.initialDate;
                 }
 
-                if ($scope.finalDate==undefined){
+                if (finalDate==undefined){
                     finalDate = now.getFullYear();
                     if ((now.getMonth()+2).toString().length==1){
                         finalDate = finalDate + "-" + "0" + (parseInt(now.getMonth())+2).toString();
@@ -124,15 +123,14 @@ BeetApp
                         finalDate = finalDate + "-" + (parseInt(now.getMonth())+2).toString();
                     }
                     finalDate = finalDate + "-01";
-                }else{
-                    finalDate = $scope.finalDate;
                 }
-
+                $scope.initialDate = initialDate;
+                $scope.finalDate = finalDate;
+                $scope.dateLabel = $scope.formatDateToLabel(initialDate);
 
                 if ($scope.moduleMode === "person") {
                     objService.getByPersonAndInterval($scope._id, initialDate, finalDate)
                         .success(function(allData) {
-                            console.log(allData.length);
                             $timeout(function(){
                                 $scope.allModuleData = allData;
                                 $rootScope.moduleLoading = false;
@@ -178,7 +176,94 @@ BeetApp
 
             }
         };
+        $scope.next = function() {
+            $rootScope.moduleLoading = true;
 
+            var arr;
+
+            var initialDate = $scope.initialDate;
+            arr = initialDate.split("-");
+            if (parseInt(arr[1])==12){
+                arr[1] = "01";
+                arr[0] = (parseInt(arr[0]) + 1).toString();
+            }else{
+                arr[1] = (parseInt(arr[1]) + 1).toString();
+            }
+            initialDate = arr[0];
+            if ((arr[1]).toString().length==1){
+                initialDate = initialDate + "-" + "0" + (parseInt(arr[1])).toString();
+            }else{
+                initialDate = initialDate + "-" + (parseInt(arr[1])).toString();
+            }
+            initialDate = initialDate + "-01";
+
+
+            var finalDate = $scope.finalDate;
+            arr = finalDate.split("-");
+            if (parseInt(arr[1])==12){
+                arr[1] = "01";
+                arr[0] = (parseInt(arr[0]) + 1).toString();
+            }else{
+                arr[1] = (parseInt(arr[1]) + 1).toString();
+            }
+            finalDate = arr[0];
+            if ((arr[1]).toString().length==1){
+                finalDate = finalDate + "-" + "0" + (parseInt(arr[1])).toString();
+            }else{
+                finalDate = finalDate + "-" + (parseInt(arr[1])).toString();
+            }
+            finalDate = finalDate + "-01";
+
+            $scope.initialDate = initialDate;
+            $scope.finalDate = finalDate;
+            $scope.dateLabel = $scope.formatDateToLabel(initialDate);
+
+            loadData(initialDate, finalDate);
+        };
+        $scope.prev = function() {
+            $rootScope.moduleLoading = true;
+
+            var arr;
+
+            var initialDate = $scope.initialDate;
+            arr = initialDate.split("-");
+            if (parseInt(arr[1])==1){
+                arr[1] = "12";
+                arr[0] = (parseInt(arr[0]) - 1).toString();
+            }else{
+                arr[1] = (parseInt(arr[1]) - 1).toString();
+            }
+            initialDate = arr[0];
+            if ((arr[1]).toString().length==1){
+                initialDate = initialDate + "-" + "0" + (parseInt(arr[1])).toString();
+            }else{
+                initialDate = initialDate + "-" + (parseInt(arr[1])).toString();
+            }
+            initialDate = initialDate + "-01";
+
+
+            var finalDate = $scope.finalDate;
+            arr = finalDate.split("-");
+            if (parseInt(arr[1])==1){
+                arr[1] = "12";
+                arr[0] = (parseInt(arr[0]) - 1).toString();
+            }else{
+                arr[1] = (parseInt(arr[1]) - 1).toString();
+            }
+            finalDate = arr[0];
+            if ((arr[1]).toString().length==1){
+                finalDate = finalDate + "-" + "0" + (parseInt(arr[1])).toString();
+            }else{
+                finalDate = finalDate + "-" + (parseInt(arr[1])).toString();
+            }
+            finalDate = finalDate + "-01";
+
+            $scope.initialDate = initialDate;
+            $scope.finalDate = finalDate;
+            $scope.dateLabel = $scope.formatDateToLabel(initialDate);
+
+            loadData(initialDate, finalDate);
+        };
         $scope.cancel = function() {
             $scope.allModuleData = undefined;
             $(".message-active").each(function(){
